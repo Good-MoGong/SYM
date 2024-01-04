0e0c7e699bae141ce293f0f83675372553d57051
Park Seo Yeon <syss220211@gmail.com>
2024-01-04 18:33:06 +0900

    feat, fix: Clean Architecture 도입 폴더링 관련 에러 수정 및 HexColorExtension파일 추가

---
+++ Projects/App/Sources/Data/Network/NetworkDemo.swift
@@ -0,0 +1,10 @@
+//
+//  NetworkDemo.swift
+//  SYM
+//
+//  Created by 박서연 on 2023/12/20.
+//  Copyright © 2023 Mogong. All rights reserved.
+//
+
+import SwiftUI
+
---
+++ Projects/App/Sources/DesignSystem/Button/ButtonDemo.swift
@@ -0,0 +1,35 @@
+//
+//  ButtonDemo.swift
+//  SYM
+//
+//  Created by 박서연 on 2024/01/04.
+//  Copyright © 2023 Mogong. All rights reserved.
+//
+
+// Demo
+import SwiftUI
+
+struct DefaultButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .frame(width: 230, height: 45)
+            .font(.system(size: 14))
+            .foregroundColor(Color(hex: "#2D2D2D"))
+            .background(Color(hex: "#FFD2D2"))
+            .cornerRadius(30)
+    }
+}
+
+struct ButtonDemo: View {
+    var body: some View {
+        VStack {
+            Button("Test") {
+                print("Test")
+            }.buttonStyle(DefaultButtonStyle())
+        }
+    }
+}
+
+#Preview {
+    ButtonDemo()
+}
