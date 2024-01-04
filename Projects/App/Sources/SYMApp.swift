93e55d4a9e00ddc8d36d7939078c210e25551962
Park Seo Yeon <syss220211@gmail.com>
2024-01-04 16:32:10 +0900

    feat: Firebase SDK 추가 완료

--- Projects/App/Sources/SYMApp.swift
+++ Projects/App/Sources/SYMApp.swift
@@ -1,8 +1,20 @@
 import SwiftUI
-import FirebaseAuth
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct SYMApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
