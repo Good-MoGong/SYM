f33e6b9db7accfb3b4f004456985de5215f696d3
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-01-05 14:05:23 +0900

    refactor: font 디자인 시스템 변수명 린트 적용

--- Projects/App/Sources/DesignSystem/Font/Font.swift
+++ Projects/App/Sources/DesignSystem/Font/Font.swift
@@ -11,54 +11,54 @@ import SwiftUI
 extension DesignSystem {
     struct FontStyles {
         /// 28 .medium
-        static let h1 = Font.system(size: 28, weight: .medium)
+        static let symH1 = Font.system(size: 28, weight: .medium)
         /// 25 .medium
-        static let h2 = Font.system(size: 25, weight: .medium)
+        static let symH2 = Font.system(size: 25, weight: .medium)
         /// 20 .medium
-        static let h3 = Font.system(size: 20, weight: .medium)
+        static let symH3 = Font.system(size: 20, weight: .medium)
         /// 18 .medium
-        static let h4 = Font.system(size: 18, weight: .medium)
+        static let symH4 = Font.system(size: 18, weight: .medium)
         /// 16 .medium
-        static let h5 = Font.system(size: 16, weight: .medium)
+        static let symH5 = Font.system(size: 16, weight: .medium)
         /// 14 .medium
-        static let body = Font.system(size: 14, weight: .medium)
+        static let symBody = Font.system(size: 14, weight: .medium)
         /// 12 .medium
-        static let small = Font.system(size: 12, weight: .medium)
+        static let symSmall = Font.system(size: 12, weight: .medium)
     }
 }
 
 struct H1TextStyle: ViewModifier {
     func body(content: Content) -> some View {
-        content.font(DesignSystem.FontStyles.h1)
+        content.font(DesignSystem.FontStyles.symH1)
     }
 }
 
 struct H2TextStyle: ViewModifier {
     func body(content: Content) -> some View {
-        content.font(DesignSystem.FontStyles.h2)
+        content.font(DesignSystem.FontStyles.symH2)
     }
 }
 
 struct H3TextStyle: ViewModifier {
     func body(content: Content) -> some View {
-        content.font(DesignSystem.FontStyles.h3)
+        content.font(DesignSystem.FontStyles.symH3)
     }
 }
 
 struct H4TextStyle: ViewModifier {
     func body(content: Content) -> some View {
-        content.font(DesignSystem.FontStyles.h4)
+        content.font(DesignSystem.FontStyles.symH4)
     }
 }
 
 struct BodyTextStyle: ViewModifier {
     func body(content: Content) -> some View {
-        content.font(DesignSystem.FontStyles.body)
+        content.font(DesignSystem.FontStyles.symBody)
     }
 }
 
 struct SmallTextStyle: ViewModifier {
     func body(content: Content) -> some View {
-        content.font(DesignSystem.FontStyles.small)
+        content.font(DesignSystem.FontStyles.symSmall)
     }
 }
--- Projects/App/Sources/DesignSystem/Font/FontDemo.swift
+++ Projects/App/Sources/DesignSystem/Font/FontDemo.swift
@@ -11,30 +11,6 @@ import SwiftUI
 struct DesignSystem {
     static let primaryButtonColor = Color.blue
     static let primaryButtonTextColor = Color.white
-
-//    struct FontStyles {
-//        static let h1 = Font.system(size: 24, weight: .bold)
-//        static let h2 = Font.system(size: 20, weight: .semibold)
-//        static let body = Font.system(size: 16, weight: .regular)
-//    }
-//}
-//
-//struct H1TextStyle: ViewModifier {
-//    func body(content: Content) -> some View {
-//        content.font(DesignSystem.FontStyles.h1)
-//    }
-//}
-//
-//struct H2TextStyle: ViewModifier {
-//    func body(content: Content) -> some View {
-//        content.font(DesignSystem.FontStyles.h2)
-//    }
-//}
-//
-//struct BodyTextStyle: ViewModifier {
-//    func body(content: Content) -> some View {
-//        content.font(DesignSystem.FontStyles.body)
-//    }
 }
 
 // 확인 예제
@@ -42,11 +18,12 @@ struct FontTest: View {
     var body: some View {
         VStack {
             Text("H1 Text")
-            // Color는 둘 중 하나의 방법으로 사용
+//             둘 중 하나의 방법으로 사용
                 .foregroundStyle(DesignSystem.ColorStyles.symGray1)
-            //  .modifier(PinkTextStyle())
-                .modifier(H1TextStyle())
-
+                .font(DesignSystem.FontStyles.symH1)
+//              .modifier(PinkTextStyle())
+//              .modifier(H1TextStyle())
+            
             Text("H2 Text")
                 .foregroundStyle(DesignSystem.ColorStyles.symGray4)
                 .modifier(H2TextStyle())
