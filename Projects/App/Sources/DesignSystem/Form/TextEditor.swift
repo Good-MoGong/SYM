59ecdfc9135e50cbe01b6c971f1ac68922fbbed5
Park Seo Yeon <syss220211@gmail.com>
2024-01-07 00:52:16 +0900

    feat: TextField, TextEditor 관련 컴포넌트 구현

---
+++ Projects/App/Sources/DesignSystem/Form/TextEditor.swift
@@ -0,0 +1,46 @@
+//
+//  TextEditor.swift
+//  SYM
+//
+//  Created by 박서연 on 2024/01/07.
+//  Copyright © 2024 Mogong. All rights reserved.
+//
+
+import SwiftUI
+
+struct CustomTextEditorStyle: ViewModifier {
+    
+    let placeholder: String
+    @Binding var text: String
+    
+    func body(content: Content) -> some View {
+        if text.isEmpty {
+            ZStack(alignment: .topLeading) {
+                content
+                    .padding(15)
+                    .background(Color.symGray1)
+                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                    .scrollContentBackground(.hidden)
+                    .font(DesignSystem.FontStyles.symBody)
+                Text(placeholder)
+                    .lineSpacing(10)
+                    .padding(20)
+                    .padding(.top, 2)
+                    .font(DesignSystem.FontStyles.symBody)
+                    .foregroundColor(Color.symGray3)
+            }
+        } else {
+            content
+                .padding(15)
+                .background(Color(UIColor.systemGray5))
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .scrollContentBackground(.hidden)
+        }
+    }
+}
+
+extension TextEditor {
+    func customStyle(placeholder: String, userInput: Binding<String>) -> some View {
+        self.modifier(CustomTextEditorStyle(placeholder: placeholder, text: userInput))
+    }
+}
