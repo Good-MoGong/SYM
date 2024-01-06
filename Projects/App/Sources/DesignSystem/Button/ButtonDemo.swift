62d753ac4e22867722c6e35a197d93b4dfb8d8e4
Jiyoung Ahn <wl950204@naver.com>
2024-01-07 00:05:54 +0900

    feat: Button 컴포넌트 생성

--- Projects/App/Sources/DesignSystem/Button/ButtonDemo.swift
+++ Projects/App/Sources/DesignSystem/Button/ButtonDemo.swift
@@ -12,10 +12,23 @@ import SwiftUI
 struct DefaultButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
-            .frame(width: 230, height: 45)
-            .font(DesignSystem.FontStyles.symH1)
-            .foregroundColor(Color(hex: "#2D2D2D"))
-            .background(Color(hex: "#FFD2D2"))
+            .frame(maxWidth: .infinity, maxHeight: 60)
+            .font(.symH3)
+            .foregroundColor(Color.symBlack)
+            .background(Color.symPink)
+            .cornerRadius(30)
+        // isPressed 통해 버튼 눌렸을 때 opacity 변경 -> pressed 시에 변경사항 협의 후 변경예정(-)
+            .opacity(configuration.isPressed ? 0.7 : 1.0)
+    }
+}
+
+struct DisabledButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity, maxHeight: 60)
+            .font(.symH3)
+            .foregroundColor(Color.symWhite)
+            .background(Color.symGray2)
             .cornerRadius(30)
     }
 }
@@ -23,10 +36,32 @@ struct DefaultButtonStyle: ButtonStyle {
 struct ButtonDemo: View {
     var body: some View {
         VStack {
-            Button("Test") {
-                print("Test")
-            }.buttonStyle(DefaultButtonStyle())
+            Button("디폴트 버튼 테스트용") {
+                print("DefaultStyle 버튼 눌림")
+            }
+            /// .buttonStyle에 적용
+            .buttonStyle(DefaultButtonStyle())
+            
+            Button("Disabled 버튼 테스트용") {
+                print("DisabledStyle 버튼 눌림")
+            }
+            .buttonStyle(DisabledButtonStyle())
+            
+            /// 버튼 나란히 생성시
+            HStack {
+                Button("테스트용") {
+                    print("DefaultStyle 버튼 눌림")
+                }
+                /// .buttonStyle에 적용
+                .buttonStyle(DefaultButtonStyle())
+                Button("테스트용") {
+                    print("DisabledStyle 버튼 눌림")
+                }
+                .buttonStyle(DisabledButtonStyle())
+            }
         }
+        /// 버튼에 padding 값 따로 주지 않음. 버튼 사용시 padding 필요
+        .padding(.horizontal, 20)
     }
 }
 
