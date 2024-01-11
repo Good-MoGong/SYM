c783f776f39db438b4d90afeb92f3e2931515fd1
Park Seo Yeon <syss220211@gmail.com>
2024-01-12 00:09:58 +0900

    feat: 카카오 및 에러 색상 추가

--- Projects/App/Sources/DesignSystem/Color/Color.swift
+++ Projects/App/Sources/DesignSystem/Color/Color.swift
@@ -27,4 +27,8 @@ extension Color {
     static let symWhite = Color.init(hex: "FFFFFF")
     /// Error 에서 사용되는 빨간색
     static let symRed = Color.init(hex: "FF5C5C")
+    /// 카카오톡 색상
+    static let kakao = Color.init(hex: "FEE500")
+    /// 말풍선 색상
+    static let bubble = Color.init(hex: "D8EBFC")
 }
