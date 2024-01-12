941163df27307ad26c26079ab4b1961e6ae38c01
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-01-12 19:48:25 +0900

    Feat: TextFieldClearButton Modifier 구현

--- Projects/App/Sources/DesignSystem/Common/CommonDemo.swift
+++ Projects/App/Sources/DesignSystem/Common/CommonDemo.swift
@@ -9,12 +9,19 @@
 import SwiftUI
 
 struct CommonDemo: View {
+    @State private var nickname = "김모공"
     var body: some View {
         VStack {
             Text("사건 테스트")
                 .setBackgroundColor(.systemPink)
             Text("모공모공의 하루 일기 테스트 문구입니다. 오늘은 24년도 1월 7일 일요일입니다.")
                 .setBackgroundColor(.grayThird)
+            
+            TextField("placeHolder", text: $nickname)
+                .clearButton(text: $nickname)
+                .padding()
+                .background(Color.symGray1)
+                .padding()
         }
     }
 }
---
+++ Projects/App/Sources/DesignSystem/Common/TextFieldClearButton.swift
@@ -0,0 +1,37 @@
+//
+//  TextFieldClearButton.swift
+//  SYM
+//
+//  Created by 변상필 on 1/12/24.
+//  Copyright © 2024 Mogong. All rights reserved.
+//
+
+import SwiftUI
+
+struct TextFieldClearButton: ViewModifier {
+    @Binding var text: String
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay {
+                if !text.isEmpty {
+                    HStack {
+                        Spacer()
+                        Button {
+                            text = ""
+                        } label: {
+                            Image(systemName: "multiply.circle.fill")
+                        }
+                        .foregroundStyle(Color.symBlack)
+                        .padding(.trailing, 10)
+                    }
+                }
+            }
+    }
+}
+
+extension View {
+    func clearButton(text: Binding<String>) -> some View {
+        modifier(TextFieldClearButton(text: text))
+    }
+}
