4f3b6dc665688c9e134e9e3d8b2abf736fe4cf75
Jiyoung Ahn <wl950204@naver.com>
2024-01-25 22:52:26 +0900

    refact: button 컴포넌트 변경 및 새로운 버튼 추가

--- Projects/App/Sources/DesignSystem/Button/Button.swift
+++ Projects/App/Sources/DesignSystem/Button/Button.swift
@@ -8,26 +8,54 @@
 
 import SwiftUI
 
+/// Default pink main button, 활성화 된 버튼
 struct PinkButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
             .frame(maxWidth: .infinity)
-            .padding(.vertical, 17)
+            .padding(.vertical, 19)
             .font(PretendardFont.h3Medium)
-            .foregroundColor(Color.symBlack)
+            .foregroundColor(Color.white)
             .background(Color.main)
             .cornerRadius(15)
     }
 }
 
+/// Sub pink button, 마이페이지에서 사용
+struct SubPinkButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 10)
+            .font(PretendardFont.h3Medium)
+            .foregroundColor(Color.white)
+            .background(Color.sub)
+            .cornerRadius(15)
+    }
+}
+
+/// Disabled button, 비활성화 된 버튼
 struct DisabledButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
             .frame(maxWidth: .infinity)
+            .padding(.vertical, 19)
+            .font(PretendardFont.h3Medium)
+            .foregroundColor(Color.symGray5)
+            .background(Color.symGray1)
+            .cornerRadius(15)
+    }
+}
+
+/// 작은 크기의 Gray button, 감정일기 기록 완료 시에 "홈" 버튼으로 사용
+struct smallGrayButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(.horizontal, 40)
             .padding(.vertical, 17)
             .font(PretendardFont.h3Medium)
-            .foregroundColor(Color.white)
-            .background(Color.symGray2)
+            .foregroundColor(Color.symGray5)
+            .background(Color.symGray1)
             .cornerRadius(15)
     }
 }
