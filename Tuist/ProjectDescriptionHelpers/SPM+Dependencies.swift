1414e030c883e672ae2dab49eeac48b26eb7b778
Park Seo Yeon <syss220211@gmail.com>
2024-02-09 15:27:00 +0900

    refactor: 의존성 방식 변경
    Tuist SPM -> Xcode SPM으로 변경하였음
    Objc 관련하여 프로젝트 빌드 설정에 linker에 "OTHER_LDFLAGS":"-Xlinker -no_warn_duplicate_libraries" 추가
    
    FirebaseSPM 모듈 헤더 못 잡는 에러는 아직 미해결

---
+++ Tuist/ProjectDescriptionHelpers/SPM+Dependencies.swift
@@ -0,0 +1,22 @@
+//
+//  SPM+Dependencies.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 박서연 on 2024/02/09.
+//
+
+import ProjectDescription
+
+public extension TargetDependency {
+    static let kingfisher: TargetDependency = .package(product: "Kingfisher")
+    static let firebaseAuth: TargetDependency = .package(product: "FirebaseAuth")
+    static let firestore: TargetDependency = .package(product: "FirebaseFirestore")
+}
+
+public extension Package {
+    
+    static let kingfisher: Package = .remote(url: "https://github.com/onevcat/Kingfisher.git",
+                                             requirement: .upToNextMajor(from: "5.15.6"))
+    static let firebase: Package = .remote(url: "https://github.com/firebase/firebase-ios-sdk.git",
+                                           requirement: .upToNextMajor(from: "10.4.0"))
+}
