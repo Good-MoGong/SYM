b7042a12063e3f84c0f8872ad413c64db1d9c2ab
Jiyoung Ahn <wl950204@naver.com>
2024-02-18 22:34:03 +0900

    Feat: RecordConditionFetch 프로토콜 생성

---
+++ Projects/App/Sources/Protocol/RecordConditionFetch.swift
@@ -0,0 +1,13 @@
+//
+//  RecordConditionFetch.swift
+//  SYM
+//
+//  Created by 안지영 on 2/18/24.
+//  Copyright © 2024 Mogong. All rights reserved.
+//
+
+import Foundation
+
+protocol RecordConditionFetch: ObservableObject {
+    func recordSpecificFetch()
+}
