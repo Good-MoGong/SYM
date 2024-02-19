8ac447bf1a80e8fa461f84b6de8aa8eaf5eda11f
Park Seo Yeon <syss220211@gmail.com>
2024-02-19 19:31:05 +0900

    feat, refactor: objc 관련 Tuist 의존성 방법 재변경, FirebaseStore에 유저정보 추가까지 완료
    - firebaseStore 사용시 objc 관련하여 Linker 오류 발생해서 다시.. tuist spm 방식으로 변경해두었습니다 ..
    - 현재 회원가입 시 닉네임 입력화면으로 이동, 입력 후 확인 버튼 탭하면 store에 유저 정보추가까지 완료

--- Projects/App/Sources/Domain/UseCases/Protocol/Services/AuthenticationService.swift
+++ Projects/App/Sources/Domain/UseCases/Protocol/Services/AuthenticationService.swift
@@ -18,7 +18,7 @@ enum AuthenticationError: Error {
     case invalidated
 }
 
-// 인증을 담당할 서비스 생성 (Services에 연관된)
+// 인증을 담당할 서비스
 protocol AuthenticationServiceType {
     // 로그인 세션
     func checkAuthenticationState() -> String?
@@ -26,16 +26,25 @@ protocol AuthenticationServiceType {
     // MARK: - Apple 로그인
     // 요청이 왔을 때 원하는 정보에 대한 범위를 request에 담아서 보내줌
     func handleSignInWithAppleRequest(_ request: ASAuthorizationAppleIDRequest) -> String
-    // 인증에 대한 성공 정보와 로그인 요청에 대한 정보를 담고 있음
+    // 인증에 대한 성공 정보와 로그인 요청에 대한 정보를 담고 있음, 함수의 끝에 유저에 대한 생성자를 리턴함
     func handleSignInWithAppleCompletion(_ authorization: ASAuthorization, none: String) -> AnyPublisher<User, ServiceError>
+    
+    // MARK: - 카카오 로그인
+    
     
     // MARK: - 로그아웃
     func logout() -> AnyPublisher<Void, ServiceError>
     
+    
+    // MARK: - Firebase 관련 해야하는 것
+    // 1) FireStore에 유저 데이터 추가
+//    func creatUserInFirebase(user: User) -> AnyPublisher<User, ServiceError>
+    // 2) 기존 회원 로그인 시 닉네임 유무 판별 ?? -> 이거는 미정 -> 처음에 닉네임 유무 판별해서 연결 뷰 관리하기 위한 닉넴 체킹 함수
 }
 
 class AuthenticationService: AuthenticationServiceType {
     // 파베를 사용해서 유저 정보 체크 후 유저 정보 추출하기
+    // 파베에 해당하는 유저가 있는지 확인하고, 있다면 id값 리턴
     func checkAuthenticationState() -> String? {
         if let user = Auth.auth().currentUser {
             return user.uid
@@ -53,11 +62,13 @@ class AuthenticationService: AuthenticationServiceType {
     
     // 완료된 작업 Futuer 생성
     func handleSignInWithAppleCompletion(_ authorization: ASAuthorization, none: String) -> AnyPublisher<User, ServiceError> {
+        // - 최종적으로 하나의 값을 생성한 후 완료되거나 실패하는 Publisher
+        // - 어떤 작업을 수행하고 비동기적으로 결과값을 방출할 수 있음
         Future { [weak self] promise in
             self?.handleSignInWithAppleCompletion(authorization, nonce: none) { result in
                 switch result {
                 case let .success(user):
-                    promise(.success(user))
+                    promise(.success(user)) // 작업 완료 시 작업값 방출해주는 operator == future
                 case let .failure(error):
                     promise(.failure(.error(error)))
                 }
@@ -114,7 +125,7 @@ extension AuthenticationService {
         }
     }
     
-    // 'AuthCredential'(Google 로그인에서 획득)을 가져와 Firebase 인증 프로세스를 완료
+    /// Firebase 인증 함수, 'AuthCredential'(Google 로그인에서 획득)을 가져와 Firebase 인증 프로세스를 완료
     private func authenticateUserWithFirebase(credential: AuthCredential, completion: @escaping (Result<User, Error>) -> Void) {
         Auth.auth().signIn(with: credential) { result, error in
             if let error {
@@ -138,6 +149,7 @@ extension AuthenticationService {
     }
 }
 
+// 프리뷰 용 프로토콜
 class StubAuthenticationService: AuthenticationServiceType {
     
     func checkAuthenticationState() -> String? {
