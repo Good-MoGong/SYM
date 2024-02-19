a406410e3a757676efb8e8f3a51736f3563ecb98
Jiyoung Ahn <wl950204@naver.com>
2024-02-19 21:15:51 +0900

    Feat: Calendar Architectures 구현

---
+++ Projects/App/Sources/Presentation/Calendar/Architectures/CalendarUseCase.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarUseCase.swift
+//  SYM
+//
+//  Created by 안지영 on 2/19/24.
+//  Copyright © 2024 Mogong. All rights reserved.
+//
+
+import Foundation
+
+
+protocol CalendarUseCaseProtocol {
+   func fetchRecord(date: String, completion: @escaping (Diary, Bool) -> Void)
+}
+
+final class CalendarUseCase: CalendarUseCaseProtocol {
+    
+    private let calendarRepository: CalendarRepository
+    
+    
+    init(calendarRepository: CalendarRepository) {
+        self.calendarRepository = calendarRepository
+    }
+    
+    func fetchRecord(date: String, completion: @escaping (Diary, Bool) -> Void) {
+        calendarRepository.fetchRecord(date: date) { diary, isSuccess in
+            completion(diary, isSuccess)
+        }
+    }
+}
--- Projects/App/Sources/Presentation/Calendar/ViewModel/CalendarViewModel.swift
+++ Projects/App/Sources/Presentation/Calendar/ViewModel/CalendarViewModel.swift
@@ -6,6 +6,24 @@
 //  Copyright © 2023 Mogong. All rights reserved.
 //
 
-// Demo
+import Foundation
 
-
+final class CalendarViewModel: RecordConditionFetch {
+    
+    private let calendarUseCase: CalendarUseCase
+    
+    @Published var recordDiary: Diary = .init(date: "", event: "", idea: "", emotions: [], action: "")
+    
+    init(calendarUseCase: CalendarUseCase) {
+        self.calendarUseCase = calendarUseCase
+    }
+    
+    func recordSpecificFetch() {
+        calendarUseCase.fetchRecord(date: recordDiary.date) { diary, isSuccess in
+            DispatchQueue.main.async {
+                self.recordDiary = diary
+                // isSuccess Bool 값 받아서 어떻게 할건지
+            }
+        }
+    }
+}
