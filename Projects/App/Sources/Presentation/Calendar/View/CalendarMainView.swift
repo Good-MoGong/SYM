ab42bb1071dea82b3ff46bfb10c662c0d15e303d
Jiyoung Ahn <wl950204@naver.com>
2024-02-22 02:00:43 +0900

    Refact: CalendarView 배경 추가

--- Projects/App/Sources/Presentation/Calendar/View/CalendarMainView.swift
+++ Projects/App/Sources/Presentation/Calendar/View/CalendarMainView.swift
@@ -12,22 +12,31 @@ struct CalendarMainView: View {
     @State var currentDate: Date = Date()
     @State var selectDate: Date = Date()
     @State private var nickname: String = "모공모공"
+    @State var isShowingOrganizeView: Bool = false
     @StateObject var calendarViewModel = CalendarViewModel(calendarUseCase: CalendarUseCase(calendarRepository: CalendarRepository()))
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                CalendarDetailView(nickname: $nickname, currentDate: $currentDate, selectDate: $selectDate, calendarViewModel: calendarViewModel)
-                    .padding(20)
-                RecordView(beforeRecord: calendarViewModel.completeRecord, nickname: nickname)
-                    .padding(.horizontal, 20)
-            }
-            .padding(.bottom, 20)
-            .scrollIndicators(.hidden)
-            .onAppear {
-                calendarViewModel.recordDiary.date = Date().formatToString()
-                calendarViewModel.recordSpecificFetch()
-                calendarViewModel.recordWholeFetch()
+            ZStack {
+                Image("CalendarBackground")
+                    .resizable()
+                    .ignoresSafeArea()
+                
+                ScrollView {
+                    CalendarDetailView(nickname: $nickname, currentDate: $currentDate, selectDate: $selectDate, calendarViewModel: calendarViewModel)
+                        .padding(20)
+                    RecordView(beforeRecord: calendarViewModel.completeRecord, nickname: nickname)
+                        .padding(.horizontal, 20)
+                }
+                .padding(.bottom, 20)
+                .scrollIndicators(.hidden)
+                .onAppear {
+                    calendarViewModel.recordDiary.date = Date().formatToString()
+                    // 오늘 날짜 데이터 페치, 이걸로 RecordView가 바뀜
+                    calendarViewModel.recordSpecificFetch()
+                    // 데이터 전체 페치
+                    calendarViewModel.recordWholeFetch()
+                }
             }
         }
     }
