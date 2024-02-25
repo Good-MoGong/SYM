34c7c5c24f7fcc8595c8bd61443847da93cf1042
Park Seo Yeon <syss220211@gmail.com>
2024-02-26 04:10:37 +0900

    feat: 카카오톡 탈퇴 구현 완료
    TODO:
    - 마이페이지 팝업에 애니메이션 추가

--- Projects/App/Sources/Presentation/MyPage/SettingView.swift
+++ Projects/App/Sources/Presentation/MyPage/SettingView.swift
@@ -9,6 +9,8 @@
 import SwiftUI
 
 struct SettingView: View {
+    private let firebaseService = FirebaseService.shared
+    
     @State private var isShowingLogoutPopup = false
     @State private var isShowingWithdrawalPopup = false
     @EnvironmentObject var authViewModel: AuthenticationViewModel
@@ -55,11 +57,18 @@ struct SettingView: View {
         })
         .popup(isShowing: $isShowingWithdrawalPopup,
                type: .doubleButton(leftTitle: "확인", rightTitle: "취소"),
-               title: "로그아웃 하시겠어요?",
+               title: "탈퇴하시겠어요?",
                boldDesc: "탈퇴 전 유의 사항",
                desc: "• 탈퇴 후 7일간은 재가입이 불가합니다. \n• 탈퇴 시 계정의 모든 정보는 삭제되며, \n   재가입후에도 복구 되지 않습니다.",
                confirmHandler: {
             print("탈퇴하기")
+            
+            guard let userId = authViewModel.userId else {
+                return
+                print("회원가입 정보 없음!!!!!! 탈퇴에러 발생!!!!!!")
+            }
+            
+            firebaseService.deleteUserData(user: userId)
             authViewModel.send(action: .unlinkKakao)
             self.isShowingWithdrawalPopup.toggle()
         },
@@ -67,6 +76,7 @@ struct SettingView: View {
             print("취소 버튼")
             self.isShowingWithdrawalPopup.toggle()
         })
+        
         .popup(isShowing: $isShowingLogoutPopup,
                type: .doubleButton(leftTitle: "확인", rightTitle: "취소"),
                title: "로그아웃 하시겠어요?",
