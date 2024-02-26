41e29c91bf26f186528ba6c710a58cd215e26924
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-02-26 23:41:46 +0900

    Refactor: 이미지 사이즈 및 프로그레스 뷰 수정

--- Projects/App/Sources/DesignSystem/Common/ProgressView.swift
+++ Projects/App/Sources/DesignSystem/Common/ProgressView.swift
@@ -9,34 +9,32 @@
 import SwiftUI
 
 struct ProgressView: View {
-    @State private var height: Double = 0.6
-
+    @State private var isAnimating = true // 점의 움직임을 제어할 상태 변수
+    
     var body: some View {
         VStack {
-            Image("SimiCurious")
             HStack(alignment: .center) {
                 Text("시미가 답장 쓰는 중")
                     .font(PretendardFont.bold18)
                 
                 ForEach(0..<3) { index in
-                    circle(height: $height, delay: Double(index + 1) * 0.3)
+                    circle(delay: Double(index) * 0.2) // 각 점마다 0.3초씩 딜레이를 줍니다.
                 }
             }
         }
     }
-
-    // 상단으로 점이 올라갔다 내려오는 뷰
+    
     @ViewBuilder
-    private func circle(height: Binding<Double>, delay: Double) -> some View {
+    private func circle(delay: Double) -> some View {
         Circle()
             .frame(width: 6, height: 6)
-            .animation(Animation.linear(duration: 0.6).repeatForever().delay(delay))
+            .animation(Animation.easeInOut.repeatForever().delay(delay))
+            .offset(y: isAnimating ? -5 : 0)
             .onAppear {
                 withAnimation(Animation.easeInOut(duration: 0.6).repeatForever()) {
-                    height.wrappedValue = 1
+                    isAnimating.toggle()
                 }
             }
-            .offset(y: height.wrappedValue == 1 ? -4 : 4)
     }
 }
 
