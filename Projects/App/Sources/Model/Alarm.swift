edcfdb7e86257ff31bb3edea201eca88be95f53a
Park Seo Yeon <syss220211@gmail.com>
2024-02-27 18:26:22 +0900

    feat: 알람 구현 1차 완료
    TODO:
    - 알람 테스팅
    - 문구 및 시간 픽스

--- Projects/App/Sources/Model/Alarm.swift
+++ Projects/App/Sources/Model/Alarm.swift
@@ -16,13 +16,18 @@ struct AlarmInfo: Hashable {
     let hour: Int
     let minute : Int
     
-    let title: String = "마음을 기록한지 \(PushNotificationService().checkUserAccessDate())일 이 지났어요"
-    let subtitle: String = subTitleList.randomElement() ?? "시미를 확인해주세요!"
+    let title: String = "오늘 하루 어떤 감정을 느끼셨나요?"// "마음을 기록한지 \(PushNotificationService().checkUserAccessDate())일 이 지났어요"
+    let subtitle: String = subTitleList.randomElement() ?? "오늘의 감정을 기록하고 내일의 나를 발견해보세요"
     let body: String = bodyList.randomElement() ?? "오늘 하루를 기록해보는건 어떤가요?"
     
     // 나중에 랜덤 데이터 들어갈 자리
     static let TitleList = ["Title First1", "Title First2", "Title First3", "Title First4"]
-    static let subTitleList = ["subTitleList1", "subTitleList2", "subTitleList3", "subTitleList4"]
+    
+    static let subTitleList = ["오늘의 감정을 기록하고 내일의 나를 발견해보세요",
+                               "어떤 감정이든 공유해주세요. 함께할 때 더 의미 있어요!",
+                               "시미는 언제든지 기록을 시작할 준비가 되어있어요!",
+                               "subTitleList4"]
+    
     static let bodyList = ["body1", "body2", "body3", "body4"]
         
     static let sampleAlarm = [AlarmInfo(weekday: 6, hour: 21, minute: 00),
