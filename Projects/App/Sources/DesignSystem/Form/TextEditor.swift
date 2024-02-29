a48c0fecca7c8b218d9c8fedf39581d71374bf49
a-jb97 <a_jb97@naver.com>
2024-03-01 04:23:38 +0900

    Feat, Refact: RecordOrganizeView 기록 편집 기능 추가, Calander HeaderView 고정

--- Projects/App/Sources/DesignSystem/Form/TextEditor.swift
+++ Projects/App/Sources/DesignSystem/Form/TextEditor.swift
@@ -60,9 +60,72 @@ struct CustomTextEditorStyle: ViewModifier {
     }
 }
 
+struct CustomTextEditorStyle2: ViewModifier {
+    
+    @Binding var text: String
+    
+    func body(content: Content) -> some View {
+        if text.isEmpty {
+            ZStack (alignment: .bottomTrailing) {
+                ZStack(alignment: .topLeading) {
+                    content
+                        .font(PretendardFont.bodyMedium)
+                        .lineSpacing(7)
+                        .padding(.horizontal, 17)
+                        .padding(.vertical, 23)
+                        .frame(maxWidth: .infinity, minHeight: 214)
+                        .background(Color.bright)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .scrollContentBackground(.hidden)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.sub, lineWidth: 1)
+                                .frame(maxWidth: .infinity, minHeight: 214)
+                        )
+                }
+                Text("\(text.count) / 200")
+                    .font(PretendardFont.smallMedium)
+                    .foregroundColor(Color.symGray4)
+                    .padding(.trailing, 10)
+                    .padding(.bottom, 10)
+            }
+        } else {
+            ZStack(alignment: .bottomTrailing) {
+                content
+                    .font(PretendardFont.bodyMedium)
+                    .lineSpacing(7)
+                    .padding(.horizontal, 17)
+                    .padding(.vertical, 23)
+                    .frame(maxWidth: .infinity, minHeight: 214)
+                    .background(Color.bright)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .scrollContentBackground(.hidden)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.sub, lineWidth: 1)
+                            .frame(maxWidth: .infinity, minHeight: 214)
+                    )
+                HStack(spacing: 0) {
+                    Text("\(text.count) ")
+                        .foregroundColor(Color.sub)
+                    Text("/ 200")
+                        .foregroundColor(Color.symGray4)
+                }
+                .padding(.trailing, 10)
+                .padding(.bottom, 10)
+                .font(PretendardFont.smallMedium)
+            }
+        }
+    }
+}
+
 extension TextEditor {
     func customStyle(placeholder: String, userInput: Binding<String>) -> some View {
         self.modifier(CustomTextEditorStyle(placeholder: placeholder, text: userInput))
+    }
+    
+    func customStyle2(userInput: Binding<String>) -> some View {
+        self.modifier(CustomTextEditorStyle2(text: userInput))
     }
 }
 
@@ -71,6 +134,7 @@ struct FormDemo: View {
     @State private var username: String = ""
     @State private var password: String = ""
     @State private var text: String = ""
+    @State private var text2: String = ""
     private let placeholder: String = "예) 친구를 만나서 영화도 보고 맛있는 것도 먹었어"
     
     var body: some View {
@@ -85,6 +149,11 @@ struct FormDemo: View {
             
             TextEditor(text: $text)
                 .customStyle(placeholder: placeholder, userInput: $text)
+                .frame(height: 200)
+                .padding()
+            
+            TextEditor(text: $text2)
+                .customStyle2(userInput: $text2)
                 .frame(height: 200)
                 .padding()
         }
