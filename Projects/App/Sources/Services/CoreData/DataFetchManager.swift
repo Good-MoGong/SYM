0931d234082144516f2bbc0d1feaf092bd7c0472
Jiyoung Ahn <wl950204@naver.com>
2024-03-01 20:24:43 +0900

    Refact: 재로그인, SNS 첫 로그인시 코어데이터, 파이어베이스 페치 / 로그아웃시 코어데이터 삭제 구현

--- Projects/App/Sources/Services/CoreData/DataFetchManager.swift
+++ Projects/App/Sources/Services/CoreData/DataFetchManager.swift
@@ -10,27 +10,32 @@ import Foundation
 import CoreData
 import FirebaseFirestore
 
-final class DataManager {
-    let firebaseManager = FirebaseManager.shared
-    let coreDataManager = CoreDataManger.shared
+final class DataFetchManager {
+    
+    static let shared = DataFetchManager()
+    
+    private let firebaseManager = FirebaseManager.shared
+    private let coreDataManager = CoreDataManger.shared
+    private let diary: Diary = .init(date: "", event: "", idea: "", emotions: [], action: "")
 
-    func fetchData(userID: String, data: Diary) async {
+    /// 로그인시, userID로 코어데이터, Firebase data fetch
+    func fetchData(userID: String) async {
         do {
             // 1. Core Data에서 데이터 조회
             let localDiaries: [DiaryEntity] = coreDataManager.retrieve(type: DiaryEntity.self)
             
             if !localDiaries.isEmpty {
                 // 1-1. Core Data에 데이터가 있으면 리턴
-                print("Core Data에서 데이터를 성공적으로 조회했습니다.")
+                print("⭐️⭐️⭐️Core Data에서 데이터를 성공적으로 조회했습니다.")
                 return
             } else {
                 // 1-2. CoreData에 데이터 없음 -> Firebase에서 데이터 조회
                 // 2. Firebase에서 데이터 조회
-                let fetchedDiaries: [Diary] = try await firebaseManager.fetchDiaryFireStore(userID: userID, data: data)
+                let fetchedDiaries: [Diary] = try await firebaseManager.fetchDiaryFireStore(userID: userID, data: diary)
                 
                 if fetchedDiaries.isEmpty {
                     // 2-1. Firebase에도 데이터가 없으면 리턴
-                    print("Firebase에 데이터가 없습니다.")
+                    print("⭐️⭐️⭐️Firebase에 데이터가 없습니다.")
                     return
                 } else {
                     // 2-2. Firebase에 데이터가 있음 -> Core Data에 Firebase 데이터 저장
@@ -47,11 +52,16 @@ final class DataManager {
                             }
                         }
                     }
-                    print("CoreData 저장 성공")
+                    print("⭐️⭐️⭐️CoreData 저장 성공")
                 }
             }
         } catch {
-            print("데이터 동기화 중 오류 발생: \(error.localizedDescription)")
+            print("⭐️⭐️⭐️데이터 동기화 중 오류 발생: \(error.localizedDescription)")
         }
+    }
+    
+    /// 코어데이터 삭제
+    func deleteCoreData() {
+        coreDataManager.deleteAll(type: DiaryEntity.self)
     }
 }
