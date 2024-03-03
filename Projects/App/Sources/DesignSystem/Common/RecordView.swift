fdc04fe13d499882c64098a22aa8c8113514bc51
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-03-03 15:48:16 +0900

    Refactor: 메인 하단의 RecordView 조건 추가(과거의 기록이 없을 경우)

--- Projects/App/Sources/DesignSystem/Common/RecordView.swift
+++ Projects/App/Sources/DesignSystem/Common/RecordView.swift
@@ -9,16 +9,19 @@
 import SwiftUI
 
 enum RecordViewText {
-    case beforeRecord
+    case beforeRecordPast
+    case beforeRecordToday
     case afterRecord
     case mypageRecord(count: Int)
     
     var stringValue: String {
         switch self {
-        case .beforeRecord:
+        case .beforeRecordPast:
+            "감정을 기록하지 않은 날이에요.\n오늘은 시미와 감정을 기록해 볼까요?"
+        case .beforeRecordToday:
             "오늘의 감정이 기록되지 않았어요\n시미가 당신을 기다리고 있어요!"
         case .afterRecord:
-            "꾸준한 감정일기는 자신을 단단히\n 만들어준답니다! 내일도 와주실거죠?"
+            "꾸준한 감정일기는 자신을 단단히\n만들어준답니다! 내일도 와주실거죠?"
         case let .mypageRecord(count):
             "\(count)개의 감정기록이 담겨있네요!\n시미가 당신의 의견을 기다리고 있어요"
         }
@@ -30,49 +33,37 @@ struct RecordView: View {
     var isShowingMainView: Bool = true
     /// 기록 전, 후를 bool로 구분
     var beforeRecord: Bool?
+    /// 감정이 기록되지 않았을 때 (과거, 오늘)
+    var isRecordPast: Bool?
     var recordCount: Int = 0
     
     @State private var isShowingRecordView: Bool = false
+    @State private var isShowingOrganizeView: Bool = false
     @State private var nickname: String = UserDefaultsKeys.nickname
     
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                if isShowingMainView {
-                    Text(beforeRecord ?? true ? "오늘의 기록" : "오늘 일기 작성 완료!")
-                        .font(PretendardFont.h4Bold)
-                        .padding(.bottom, 12)
-                } else {
-                    NavigationLink {
-                        MyAccountInfo()
-                    } label: {
-                        HStack {
-                            Text("\(nickname)님")
-                                .font(PretendardFont.h4Bold)
-                            Image(systemName: "chevron.right")
-                        }
-                    }
-                    .buttonStyle(PlainButtonStyle())
+                Text(titleText)
+                    .font(.bold(18))
                     .padding(.bottom, 12)
-                }
                 
-                VStack(alignment: .leading) {
-                    Text(isShowingMainView ? (beforeRecord ?? false ?
-                                              RecordViewText.beforeRecord.stringValue : RecordViewText.afterRecord.stringValue
-                                             ) :
-                            RecordViewText.mypageRecord(count: recordCount).stringValue)
-                }
-                .lineSpacing(8)
-                .font(PretendardFont.bodyMedium)
-                .fixedSize(horizontal: true, vertical: false)
-                .padding(.bottom, 12)
-                if beforeRecord ?? true {
+                Text(contentText)
+                    .lineSpacing(8)
+                    .font(.medium(14))
+                    .fixedSize(horizontal: true, vertical: false)
+                    .padding(.bottom, 12)
+                
+                if isRecordPast ?? true {
                     Button {
                         if isShowingMainView && beforeRecord ?? true {
                             isShowingRecordView = true
+                        } else {
+                            isShowingOrganizeView = true
+                            
                         }
                     } label: {
-                        Text(isShowingMainView ? (beforeRecord ?? false ? "감정 기록하기" : "기록 보러가기") : "시미에게 의견 보내기")
+                        Text(actionButtonText)
                             .font(isShowingMainView ? PretendardFont.h4Bold : PretendardFont.h5Medium)
                             .padding(.vertical, isShowingMainView ? -5 : 5)
                     }
@@ -85,30 +76,61 @@ struct RecordView: View {
             
             Spacer()
             
-            // 기록 전
-            if beforeRecord ?? false {
-                Image("SimiSad")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: .symWidth * 0.3)
-            } else {
-                // 기록 후
-                Image("SimiSmile")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: .symWidth * 0.3)
-            }
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: .symWidth * 0.3)
         }
-        .padding(.horizontal, 20)
+        .padding(.horizontal, 14)
         .padding(.vertical, 18)
         .background(
             RoundedRectangle(cornerRadius: 15)
                 .fill(Color.bright)
         )
     }
+    
+    private var titleText: String {
+        if isShowingMainView {
+            if let beforeRecord = beforeRecord {
+                return beforeRecord ? "기록이 없어요!" : "오늘의 기록"
+            } else {
+                return "감정일기 작성 완료"
+            }
+        } else {
+            return "\(nickname)님"
+        }
+    }
+    
+    private var contentText: String {
+        if isShowingMainView {
+            if beforeRecord == true {
+                if let isRecordPast = isRecordPast {
+                    return !isRecordPast ? RecordViewText.beforeRecordPast.stringValue : RecordViewText.beforeRecordToday.stringValue
+                } else {
+                    return ""
+                }
+            } else {
+                return RecordViewText.afterRecord.stringValue
+            }
+        } else {
+            return RecordViewText.mypageRecord(count: recordCount).stringValue
+        }
+    }
+    
+    private var actionButtonText: String {
+        if isShowingMainView {
+            return beforeRecord ?? false ? "감정 기록하기" : "기록 보러가기"
+        } else {
+            return "시미에게 의견 보내기"
+        }
+    }
+    
+    private var imageName: String {
+        return beforeRecord ?? false ? "SimiSad" : "SimiSmile"
+    }
 }
 
 #Preview {
-    RecordView(beforeRecord: true)
+    RecordView(beforeRecord: true, isRecordPast: true)
         .environmentObject(AuthenticationViewModel(container: DIContainer(services: Services())))
 }
