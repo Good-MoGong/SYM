6276b17032187f9f730f10aa1261517ebcc0316e
Park Seo Yeon <syss220211@gmail.com>
2024-03-14 15:05:32 +0900

    style: 레거시 파일 및 폴더 삭제

--- Projects/App/Sources/Data/Repositories/RepoDemo.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RepoDemo.swift
-//  SYM
-//
-//  Created by 박서연 on 2023/12/20.
-//  Copyright © 2023 Mogong. All rights reserved.
-//
-
-import SwiftUI
--- Projects/App/Sources/Services/Login&Authentication/PushNotificationService.swift
+++ Projects/App/Sources/Services/Login&Authentication/PushNotificationService.swift
@@ -30,38 +30,6 @@ class PushNotificationService: NSObject, PushNotificationServiceType {
             }
         }
     }
-    
-    // 알람 세팅 함수 테스트용
-//    func settingPushNotification() {
-//        let center = UNUserNotificationCenter.current()
-//        
-//        // 알림 콘텐츠 생성
-//        let content = UNMutableNotificationContent()
-//        content.title = AlarmInfo.title
-//        content.body = AlarmInfo.body
-//        
-//        // 반복되는 알림 예약을 위한 반복 횟수
-//        let repeatCount = 5
-//        
-//        // 반복문을 사용하여 여러 알림을 예약
-//        for i in 1...repeatCount {
-//            // i분 후의 날짜 계산
-//            let triggerDate = Calendar.current.date(byAdding: .second, value: i * 30, to: Date())!
-//            
-//            // 알림 요청 생성
-//            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: triggerDate.timeIntervalSinceNow, repeats: false)
-//            let request = UNNotificationRequest(identifier: "\(i)Later", content: content, trigger: trigger)
-//            
-//            // 알림 예약
-//            center.add(request) { error in
-//                if let error = error {
-//                    print("⏰ 알림 설정 실패: \(error.localizedDescription)")
-//                } else {
-//                    print("⏰ 알림 설정 완료\(i) later")
-//                }
-//            }
-//        }
-//    }
 
     // 알람 세팅 함수
     func settingPushNotification() {
